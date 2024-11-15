3ffbc464529afde8e6a600316f046be168201165
Luxmi Agnes Palma <luxmiagnes.palma@learnet.se>
2024-11-15 18:17:42 +0100

    push this line

--- LUX  KASSA/ErrorHandler.cs
+++ LUX  KASSA/ErrorHandler.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LUX__KASSA
+namespace LUX_Cash_Register
+
 {
     public class ErrorHandler : IErrorHandler
     {
--- LUX  KASSA/IErrorHandler.cs
+++ LUX  KASSA/IErrorHandler.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LUX__KASSA
+namespace LUX_Cash_Register
 {
     public interface IErrorHandler
     {
